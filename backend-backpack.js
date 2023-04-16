16bde929b879bf2c61270acc1c984b1da3e57f38
Md Jhohirul Islam <91692259+MuhammadShohagIslam@users.noreply.github.com>
2023-04-16 20:22:08 +0300

    added some question

--- backend-backpack.js
+++ backend-backpack.js
@@ -2,4 +2,11 @@
   *** What is Back End Development ***
     => Back End Developer means which fully focus on server side software.
       Which user does not see. Back End Developer concern on API, Aritechure, Database, Security, and so on.
+      
+   *** What is the API ***
+    => API is an application programming interface that set of rules which have to must follow if want to communitcate with other software
+    system.
+    
+   *** What is the RESTfull API ***
+    => RestFull API is a interface which help tp exchange information secure way over internet one software to another software system.
 */
