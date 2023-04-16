51c8677177f4d680809ed3bbd3c099ba78f47b79
Md Jhohirul Islam <91692259+MuhammadShohagIslam@users.noreply.github.com>
2023-04-16 20:42:57 +0300

    added graphql some question

--- backend-backpack.js
+++ backend-backpack.js
@@ -10,3 +10,11 @@
    *** What is the RESTfull API ***
     => RestFull API is a interface which help tp exchange information secure way over internet one software to another software system.
 */
+/*
+  *** What is GraphQL ***
+   => Graphql is the open source query language and data manupulation language of APIs.
+  *** What is the difference between graphql vs RestAPI ***
+    => Graphql architecture is client driven, rest api server driven
+    => Graphql organize term of schema and type system, rest api organized terms of Endpoint
+    => Query so fast, is the take time.
+*/
